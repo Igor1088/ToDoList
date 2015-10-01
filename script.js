d3f7319161cd788488e334b7da1f9b2b0e9e4f7d
Igor1088 <igor.puzovic@yahoo.com>
2015-10-01 22:22:58 +0200

    remove list item on click

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ var list = document.querySelector('#list'),
 document.querySelector('#btn').addEventListener('click', function(ev){
 	var text = item.value;
 	if(text !== ''){
-	list.innerHTML += '<li class="toDoItems">' +text+ '</li>';
+	list.innerHTML += '<li class="toDoItems"> <button onclick="remove()">X</button>  ' +text+ '</li>';
 	item.value = '';
     }
 	ev.preventDefault();
 
 }, false);	
+
+
+function remove(){
+	var listItems = document.getElementsByTagName("li");
+	for(var i = 0; i < listItems.length; i++){
+		listItems[i].onclick = function(){
+			this.parentNode.removeChild(this);
+		}
+	}
+
+	
+}
 
 
 
